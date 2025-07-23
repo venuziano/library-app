baff5a1e3bafb5f31020dd0673d9b7181faee5a2
Rafael Rodrigues <raf4.rrs@gmail.com>
2025-07-23 05:56:21 -0300

    ADD: Adding github action; Moved terraform files into cloud folder; Adding eks deployment file

--- src/infrastructure/cloud/terraform/variables.tf
+++ src/infrastructure/cloud/terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "sa-east-1"
 }
 
 variable "project_name" {
@@ -26,7 +26,7 @@ variable "vpc_cidr" {
 variable "availability_zones" {
   description = "Availability zones"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b"]
+  default     = ["sa-east-1a", "sa-east-1b"]
 }
 
 variable "public_subnets" {
